f258c407e650ac46210e4243e347afaa1b3c0fc7
becomon <ariana.blanco@utec.edu.pe>
2025-11-07 15:48:58 -0500

    Update temporizador.c

--- temporizador.c
+++ temporizador.c
@@ -6,17 +6,17 @@
 
 bool flag = false;      
 int estado = 0; 
-
+//En el manejador se define el flag y el estado
 void manejador(int sig) {
-    printf("Estado: %d y flag: %d \n", estado, flag);
-    if (estado == 0) {
+    printf("Estado: %d y flag: %d \n", estado, flag); //AL tener la variable estado = 0, se imprimir primero las letras
+    if (estado == 0) { 
         flag = true;
         estado = 1;
-        alarm(2);
+        alarm(2); //Se actualiza el tiempo a 2 y procede a imprimir números 
     } else {
         flag = false;
         estado = 0;
-        alarm(5);
+        alarm(5); //Se actualiza el tiempo a 5 y procede a imprimir letras
     }
 }
 
@@ -24,6 +24,7 @@ int main() {
     signal(SIGALRM, manejador);
     int i = 0;
     char c = 'A';
+    //Se crea el primer temporizador de 5 segundos
     alarm(5);
     while (1) {
         if (flag) {
